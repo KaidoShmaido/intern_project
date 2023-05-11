54a602d7ee9c4a916eaf3548effb7b1852113eed
KaidoShmaido <113863840+KaidoShmaido@users.noreply.github.com>
2023-05-11 22:36:17 +0100

    added bootstrap

--- intern_project/resources/views/layouts/app.blade.php
+++ intern_project/resources/views/layouts/app.blade.php
@@ -12,6 +12,11 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    <!-- Styles -->
+
+    <link  href="{{asset('frontend/css/bootstrap5.css')}}" rel="stylesheet">
+    <link  href="{{asset('frontend/css/custom.css')}}" rel="stylesheet">
+
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
@@ -76,5 +81,8 @@
             @yield('content')
         </main>
     </div>
+
+    <!--scripts-->
+    <script src="{{asset('/frontend/js/bootstrap.bundle.min.js')}}"></script>
 </body>
 </html>

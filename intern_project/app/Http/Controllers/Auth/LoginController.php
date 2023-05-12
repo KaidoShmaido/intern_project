dbc83e425fee96f7a133d7afb0ca983839022b99
KaidoShmaido <113863840+KaidoShmaido@users.noreply.github.com>
2023-05-12 08:36:58 +0100

    .

--- intern_project/app/Http/Controllers/Auth/LoginController.php
+++ intern_project/app/Http/Controllers/Auth/LoginController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
